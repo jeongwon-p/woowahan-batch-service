0016a338a03a833dd3f9b1bceff4b240997addc5
desktop <jeongwon.park.0314@gmail.com>
2021-05-25 23:26:05 +0900

    Start project

---
+++ src/main/java/com/woowahan/woowahanbatchservice/WoowahanBatchServiceApplication.java
@@ -0,0 +1,13 @@
+package com.woowahan.woowahanbatchservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WoowahanBatchServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WoowahanBatchServiceApplication.class, args);
+    }
+
+}
